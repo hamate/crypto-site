d5687169dacc338014dd1cee5431137d4376f2b8
hamate <matehajdu2@gmail.com>
2020-12-09 09:24:31 +0100

    fixed redux actions

--- frontend/src/actions/watchListAction.js
+++ frontend/src/actions/watchListAction.js
@@ -1,4 +1,23 @@
+import coinDataFetch from '../utilities/coinDataFetch';
+
 export const setWatchListAction = (watchListData) => ({
   type: 'SET_WATCHLIST',
   payload: watchListData,
 });
+
+export function setWatchListAsyncAction(coinId) {
+  return async (dispatch) => {
+    try {
+      const coinFetchResult = await coinDataFetch(
+        '/coins/markets?' +
+          new URLSearchParams({
+            vs_currency: 'usd',
+            ids: coinId,
+          })
+      );
+      return dispatch(setWatchListAction(coinFetchResult));
+    } catch {
+      // return dispatch(setCoinFetchError('Can\'t load coins. Please refresh the page!'));
+    }
+  };
+}
--- frontend/src/components/CoinSummary/CoinList.js
+++ frontend/src/components/CoinSummary/CoinList.js
@@ -1,37 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import coinDataFetch from '../../utilities/coinDataFetch';
-import { setWatchListAction } from '../../actions/watchListAction';
+import { setWatchListAsyncAction } from '../../actions/watchListAction';
 import Coin from './Coin';
 
 function CoinList() {
   const [coins, setCoins] = useState(['bitcoin', 'ethereum', 'ripple']);
-  const [coinsData, setCoinsData] = useState([]);
   const dispatch = useDispatch();
+  const coinsResult = useSelector((state) => state.watchList.coins);
 
   useEffect(() => {
-    dispatch(setWatchListAction(coins.join(',')));
-    async function fetchCoinAPI() {
-      const coinFetch = await coinDataFetch(
-        '/coins/markets?' +
-          new URLSearchParams({
-            vs_currency: 'usd',
-            ids: coins,
-          })
-      );
-      setCoinsData(coinFetch);
-    }
-    fetchCoinAPI();
+    dispatch(setWatchListAsyncAction(coins.join(',')));
   }, []);
-  console.log(coinsData);
+  console.log(coinsResult);
 
   return (
-    <div>
-      <ul className="coinlist">
-        {coinsData.map((coin) => (
-          <Coin key={coin.id} />
-        ))}
-      </ul>
+    <div className="coin-list container">
+      {coinsResult !== undefined ? (
+        <ul className="coinlist">
+          {coinsResult.map((coin) => (
+            <Coin key={coin.id} />
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
